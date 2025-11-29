acdb5e6bea6669f666d051757e4696c3d5755e7c
VyacheslavKoryakin <kkvvvv02@gmail.com>
2025-11-29 19:11:40 +0300

    Add files via upload

---
+++ ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.vkoryakin.diplom.techresource;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TechResourceApplication.class);
+    }
+
+}
---
+++ TechResourceApplication.java
@@ -0,0 +1,13 @@
+package com.vkoryakin.diplom.techresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechResourceApplication.class, args);
+    }
+
+}
