8edae51d6e2dd4101bd0b27575fbfed190cffd2b
VyacheslavKoryakin <kkvvvv02@gmail.com>
2025-11-30 21:51:32 +0300

    change driverRepository

--- DriverRepository.java
+++ DriverRepository.java
@@ -2,6 +2,7 @@ package com.vkoryakin.diplom.techresource.Repository;
 
 import com.vkoryakin.diplom.techresource.Entity.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -19,4 +20,10 @@ public interface DriverRepository extends JpaRepository<Driver, Long> {
 
     @Query(value="select document from driver where id =:driverId", nativeQuery=true)
     String findDocById(long driverId);
+
+
+    @Query(value = "insert into driver (name, document, brand_id) values(:driverName, :driverDocument, :driverId)", nativeQuery = true)
+     void saveDriver(String driverName, String driverDocument, long driverId) ;
+
+
 }
